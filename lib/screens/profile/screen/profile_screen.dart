d0cc6bbedf8034494777a311f1a1720e2cd39363
tu pham <panhtu0902@gmail.com>
2025-04-07 23:46:07 +0700

    feat: update project

--- lib/screens/profile/screen/profile_screen.dart
+++ lib/screens/profile/screen/profile_screen.dart
@@ -39,31 +39,41 @@ class _ProfileScreenState extends State<ProfileScreen> {
   }
 
   Widget _buildUserInfo() {
-    return const Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+    return const Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Text(
-          'Pham Anh Tu',
-          style: TextStyle(
-            fontSize: 34,
-            fontWeight: FontWeight.bold,
-          ),
+        CircleAvatar(
+          radius: 30,
+          backgroundImage: AssetImage('assets/images/person.jpg'),
         ),
-        SizedBox(height: 8),
-        Text(
-          'panhtu0902@mail.com',
-          style: TextStyle(
-            fontSize: 16,
-            color: Colors.grey,
-          ),
-        ),
+        SizedBox(width: 20),
+        Column(
+          children: [
+            Text(
+              'Pham Anh Tu',
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 2),
+            Text(
+              'panhtu0902@mail.com',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey,
+              ),
+            ),
+          ],
+        )
       ],
     );
   }
 
   Widget _buildOrderSection() {
     final menuItems = [
-      {'title': 'Already have 3 orders', 'trailing': ''},
+      {'title': 'My orders', 'trailing': 'Already have 3 orders'},
       {'title': 'Shipping addresses', 'trailing': '1 addresses'},
       {'title': 'Payment methods', 'trailing': 'Momo 0395****38'},
       {'title': 'Promocodes', 'trailing': 'You have special promocodes'},
@@ -76,22 +86,24 @@ class _ProfileScreenState extends State<ProfileScreen> {
       itemCount: menuItems.length,
       separatorBuilder: (context, index) => const Divider(height: 0),
       itemBuilder: (context, index) => ListTile(
-        contentPadding: const EdgeInsets.symmetric(vertical: 12),
+        contentPadding: const EdgeInsets.symmetric(vertical: 6),
         title: Text(
           menuItems[index]['title']!,
           style: TextStyle(
             fontSize: 16,
             color: Colors.grey[800],
-            fontWeight: index == 0 ? FontWeight.normal : FontWeight.w500,
+            fontWeight: FontWeight.w700,
           ),
         ),
-        trailing: Text(
+        subtitle: Text(
           menuItems[index]['trailing']!,
           style: TextStyle(
             color: Colors.grey[600],
-            fontSize: 14,
+            fontSize: 12,
           ),
         ),
+        trailing:
+            IconButton(onPressed: () {}, icon: const Icon(Icons.arrow_forward_ios)),
         onTap: () {},
       ),
     );
