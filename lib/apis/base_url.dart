fa990bfcb9cc2549429493668747b5aed53d2d82
tu pham <panhtu0902@gmail.com>
2025-04-17 23:50:53 +0700

    feat: update api

--- lib/apis/base_url.dart
+++ lib/apis/base_url.dart
@@ -3,5 +3,7 @@
 // const String baseUrl = "http://172.16.0.2:5000/"; // vpn
 // const String baseUrl = "http://192.168.1.43:5000/"; // wifi home
 // const String baseUrl = "http://localhost:5000";
-const String baseUrl = "http://192.168.1.96:5000/";
+// const String baseUrl = "http://192.168.1.31:5000/";
 // const String baseUrl = "http://10.0.2.2:5000/"; // android emulator
+// const String baseUrl = "http://192.168.1.197:5000/";
+const String baseUrl = "https://shopcom-be.onrender.com/";
