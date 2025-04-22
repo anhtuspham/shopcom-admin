7fe66638f5514fdd98007f7c59b379c24df08584
tu pham <panhtu0902@gmail.com>
2025-04-23 01:41:48 +0700

    feat: add headerColor, valueColor properties to custom header info widget

--- lib/widgets/custom_header_info.dart
+++ lib/widgets/custom_header_info.dart
@@ -7,15 +7,19 @@ class CustomHeaderInfo extends StatelessWidget {
   final double? fontSize;
   final FontWeight? headerFontWeight;
   final FontWeight? valueFontWeight;
+  final Color? headerColor;
+  final Color? valueColor;
 
   const CustomHeaderInfo(
       {super.key,
-        required this.title,
-        required this.value,
-        this.headerWidth = 150,
-        this.fontSize = 14,
-        this.headerFontWeight = FontWeight.w400,
-        this.valueFontWeight = FontWeight.w400});
+      required this.title,
+      required this.value,
+      this.headerWidth = 150,
+      this.fontSize = 14,
+      this.headerFontWeight = FontWeight.w400,
+      this.valueFontWeight = FontWeight.w400,
+      this.headerColor,
+      this.valueColor});
 
   @override
   Widget build(BuildContext context) {
@@ -26,14 +30,19 @@ class CustomHeaderInfo extends StatelessWidget {
             width: headerWidth,
             child: Text('$title: ',
                 style: TextStyle(
-                    fontSize: fontSize, fontWeight: headerFontWeight))),
+                    fontSize: fontSize,
+                    fontWeight: headerFontWeight,
+                    color: headerColor))),
         Expanded(
             child: Text(
-              value,
-              textAlign: TextAlign.end,
-              style: TextStyle(fontSize: fontSize, fontWeight: valueFontWeight),
-              softWrap: true,
-            ))
+          value,
+          textAlign: TextAlign.end,
+          style: TextStyle(
+              fontSize: fontSize,
+              fontWeight: valueFontWeight,
+              color: valueColor),
+          softWrap: true,
+        ))
       ],
     );
   }
